6d13b6970cb84aa136001d1b33ff1b498980eee3
MercyKorir <mcthegreat78@gmail.com>
2025-01-06 13:41:50 +0300

    chore: update to include fav icon and title as well as other meta data

--- src/components/home/index.tsx
+++ src/components/home/index.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Head from "next/head";
 import Ellipse from "../../../public/images/Ellipsebg.svg";
 import Ellipse2 from "../../../public/images/Ellipsebg2.svg";
 import MercyLogo from "../../../public/images/Logo.png";
@@ -9,54 +10,63 @@ interface HomeSectionProps {}
 
 const HomeSection: React.FC<HomeSectionProps> = () => {
   return (
-    <div className={styles.homeContainer}>
-      <div className={styles.homeContent}>
-        <div className={styles.leftText}>
-          <h1>Hello.</h1>
-          <p>
-            Web developer by day, internet jester by night. Together, we'll
-            craft something truly one-of-a-kind.
-          </p>
-        </div>
-        <div className={styles.centerItems}>
-          <span className={styles.ellipseOuterBg}>
-            <Image
-              width={500}
-              height={500}
-              src={Ellipse}
-              alt="Ellipse Image Background"
-              className={styles.outerEllipseImg}
-            />
-          </span>
-          <span className={styles.ellipseInnerBg}>
-            <Image
-              width={320}
-              height={320}
-              src={Ellipse2}
-              alt="Ellipse Image Background 2"
-              className={styles.innerEllipseImg}
-            />
-          </span>
-          <span className={styles.userImgContainer}>
-            <Image
-              width={300}
-              height={300}
-              src={MercyLogo}
-              alt="Mercy Image"
-              className={styles.userImg}
-            />
-          </span>
-        </div>
-        <div className={styles.rightItems}>
-          <h3>Full Stack Web Development</h3>
-          <h3>Front End Development</h3>
-          <h3>Back End Development</h3>
-          <h3>Web Enhancement</h3>
-          <h3>Cross-Platform Development</h3>
-          <h3>Freelance Web Development</h3>
+    <>
+      <Head>
+        <title>Home - My Portfolio</title>
+        <meta
+          name="description"
+          content="Welcome to my portfolio. Discover my work and projects."
+        />
+      </Head>
+      <div className={styles.homeContainer}>
+        <div className={styles.homeContent}>
+          <div className={styles.leftText}>
+            <h1>Hello.</h1>
+            <p>
+              Web developer by day, internet jester by night. Together, we'll
+              craft something truly one-of-a-kind.
+            </p>
+          </div>
+          <div className={styles.centerItems}>
+            <span className={styles.ellipseOuterBg}>
+              <Image
+                width={500}
+                height={500}
+                src={Ellipse}
+                alt="Ellipse Image Background"
+                className={styles.outerEllipseImg}
+              />
+            </span>
+            <span className={styles.ellipseInnerBg}>
+              <Image
+                width={320}
+                height={320}
+                src={Ellipse2}
+                alt="Ellipse Image Background 2"
+                className={styles.innerEllipseImg}
+              />
+            </span>
+            <span className={styles.userImgContainer}>
+              <Image
+                width={300}
+                height={300}
+                src={MercyLogo}
+                alt="Mercy Image"
+                className={styles.userImg}
+              />
+            </span>
+          </div>
+          <div className={styles.rightItems}>
+            <h3>Full Stack Web Development</h3>
+            <h3>Front End Development</h3>
+            <h3>Back End Development</h3>
+            <h3>Web Enhancement</h3>
+            <h3>Cross-Platform Development</h3>
+            <h3>Freelance Web Development</h3>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
--- src/components/work/index.tsx
+++ src/components/work/index.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import Head from "next/head";
 import WorkCard from "./WorkCard";
 import ProjectCard from "./ProjectCard";
 import ModalCard from "./ModalCard";
@@ -16,7 +17,7 @@ const WorkSection: React.FC<WorkSectionProps> = () => {
   const [selectedData, setSelectedData] = useState<
     WorkData | ProjectData | null
   >(null);
-  const basePath = process.env.BASE_PATH || '';
+  const basePath = process.env.BASE_PATH || "";
 
   const handleShowWorkDetails = (work: WorkData) => {
     setSelectedData(work);
@@ -34,66 +35,75 @@ const WorkSection: React.FC<WorkSectionProps> = () => {
   };
 
   return (
-    <div className={styles.workContainer}>
-      <div className={styles.workContent}>
-        <div
-          className={styles.introContainer}
-          style={{
-            backgroundImage: `url('${basePath}/images/otherbg.svg')`
-          }}
-        >
-          <div className={styles.introContent}>
-            <div className={styles.introHeader}>
-              <div className={styles.introHeaderContent}>
-                <h1>The Pixel Playground</h1>
-                <p>
-                  Welcome to the showcase of my digital artistry. Here, you'll
-                  find a curated collection of projects that have challenged me,
-                  inspired me, and ultimately fueled my growth as a web
-                  developer. Prepare to be captivated by the fusion of
-                  functionality and design, as I unveil the stories behind each
-                  creation
-                </p>
+    <>
+      <Head>
+        <title>My Work - My Portfolio</title>
+        <meta
+          name="description"
+          content="Learn more about my work and my projects."
+        />
+      </Head>
+      <div className={styles.workContainer}>
+        <div className={styles.workContent}>
+          <div
+            className={styles.introContainer}
+            style={{
+              backgroundImage: `url('${basePath}/images/otherbg.svg')`,
+            }}
+          >
+            <div className={styles.introContent}>
+              <div className={styles.introHeader}>
+                <div className={styles.introHeaderContent}>
+                  <h1>The Pixel Playground</h1>
+                  <p>
+                    Welcome to the showcase of my digital artistry. Here, you'll
+                    find a curated collection of projects that have challenged
+                    me, inspired me, and ultimately fueled my growth as a web
+                    developer. Prepare to be captivated by the fusion of
+                    functionality and design, as I unveil the stories behind
+                    each creation
+                  </p>
+                </div>
+              </div>
+              <div className={styles.workExpContainer}>
+                <h4 className={styles.workExpTitle}>Work Experience</h4>
+                <div className={styles.workExpList}>
+                  {workData.map((work, index) => (
+                    <WorkCard
+                      key={index}
+                      work={work}
+                      clickFunction={() => handleShowWorkDetails(work)}
+                    />
+                  ))}
+                </div>
               </div>
             </div>
-            <div className={styles.workExpContainer}>
-              <h4 className={styles.workExpTitle}>Work Experience</h4>
-              <div className={styles.workExpList}>
-                {workData.map((work, index) => (
-                  <WorkCard
+          </div>
+          <div
+            className={styles.projectsContainer}
+            style={{
+              backgroundImage: `url('${basePath}/images/otherbg2.svg')`,
+            }}
+          >
+            <div className={styles.projectsContent}>
+              <h4 className={styles.projectsTitle}>My Projects</h4>
+              <div className={`${styles.projectsList}`} ref={containerRef}>
+                {projectData.map((project, index) => (
+                  <ProjectCard
                     key={index}
-                    work={work}
-                    clickFunction={() => handleShowWorkDetails(work)}
+                    project={project}
+                    clickFunction={() => handleShowProjectDetails(project)}
                   />
                 ))}
               </div>
             </div>
           </div>
         </div>
-        <div
-          className={styles.projectsContainer}
-          style={{
-            backgroundImage: `url('${basePath}/images/otherbg2.svg')`
-          }}
-        >
-          <div className={styles.projectsContent}>
-            <h4 className={styles.projectsTitle}>My Projects</h4>
-            <div className={`${styles.projectsList}`} ref={containerRef}>
-              {projectData.map((project, index) => (
-                <ProjectCard
-                  key={index}
-                  project={project}
-                  clickFunction={() => handleShowProjectDetails(project)}
-                />
-              ))}
-            </div>
-          </div>
-        </div>
+        {showModal && (
+          <ModalCard selectedData={selectedData} closeFn={handleCloseModal} />
+        )}
       </div>
-      {showModal && (
-        <ModalCard selectedData={selectedData} closeFn={handleCloseModal} />
-      )}
-    </div>
+    </>
   );
 };
 
